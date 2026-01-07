d03c434a869fb834c37be095902c2d3a40d6401d
Petr Glaser <syreanis+1@gmail.com>
2026-01-07 23:22:15 +0100

    test: forbid debug helper usage

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -18,6 +18,16 @@ arch('form requests')
     ->expect('App\\Http\\Requests')
     ->toExtend('Illuminate\\Foundation\\Http\\FormRequest');
 
+arch('debug helpers')
+    ->expect('App')
+    ->not->toUse([
+        'dd',
+        'dump',
+        'ray',
+        'var_dump',
+        'print_r',
+    ]);
+
 arch('controllers')
     ->expect('App\Http\Controllers')
     ->not->toBeUsed();
